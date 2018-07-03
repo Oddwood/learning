ad2eb45062c705b80867b76e659d6f2269e43938
Marco Neuhaus <me@marconeuhaus.ch>
2018-07-03 20:57:10 +0200

    Added Flask summary
    How to use the dockerized flask application

--- python/flask/code/app.py
+++ python/flask/code/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_whale():
-    return ("HEKK")
+    return ("Hello From Flask within Docker!!")
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
