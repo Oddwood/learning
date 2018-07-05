5405dd5f818da16d7323e3ecf8d6d3ce2fcbb4f2
Marco Neuhaus <me@marconeuhaus.ch>
2018-07-05 21:37:55 +0200

    Added a user and user profile page
    This features has implemented an API call to a dummy API service and it retrieves some Dat

--- python/flask/code/app.py
+++ python/flask/code/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+import requests as api_caller
+import json
+
 from utils.lister import Lister
 
 app = Flask(__name__)
@@ -8,7 +11,8 @@ def index():
     
     listOfLinks = [
         'loop',
-        'loop/15'
+        'loop/15',
+        'users'
     ]
     
     linkList=''
@@ -22,8 +26,58 @@ def index():
 def loop():
     return "<ul>"+Lister.getList(5)+"</ul>"
 
+@app.route('/loop/', defaults={'numOfListElements': None})
 @app.route('/loop/<int:numOfListElements>')
-def loopofnum(numOfListElements: int) ->str:    
+def loopofnum(numOfListElements: int) ->str:
+    
+    if numOfListElements is None:
+        return redirect(url_for('loop'))
+
     if numOfListElements > 50:
-        raise Exception("Input is too high")
+        return "I see what you did there"
+    
     return "<ul>"+Lister.getList(numOfListElements)+"</ul>"
+
+@app.route('/users/')
+def users():
+    users = api_caller.get("https://jsonplaceholder.typicode.com/users/").json()
+
+    out = ''
+    for user in users:
+        out += """
+            name: {}<br />
+            email: {}<br />
+            adress: {}<br />
+            website: <a href="http://www.{website}">{website}</a>
+            <br /><br />
+            <b>Profile: <a href="{path}{id}">View profile</a></b>
+            <br /><br />
+            {delmiter}
+            <br />
+        """.format(
+            user['name'],
+            user['email'],
+            user['address']['city'],
+            website = user['website'],
+            id = user['id'],
+            path = request.path,
+            delmiter='---'*10 
+        )
+
+    return out
+
+@app.route('/users/<int:user_id>')
+def user_profile(user_id):
+    url = "https://jsonplaceholder.typicode.com/users/" + str(user_id)
+    user = api_caller.get(url).json()
+
+    out = """
+        <h1>Hello {}</h1><br />
+        <p>
+           Your username is: {} 
+        </p>
+        <a href="/call">Go Back</a>
+        
+    """.format(user['name'], user['username'])
+
+    return out
