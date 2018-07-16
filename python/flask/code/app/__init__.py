7233d46ec0589b536975dd90ad1978230918520b
Marco Neuhaus <me@marconeuhaus.ch>
2018-07-16 20:55:13 +0200

    Added basic setup for postgress
    Tried to connect to DB. Does not work yet... Please help

--- python/flask/code/app/__init__.py
+++ python/flask/code/app/__init__.py
@@ -1,5 +1,53 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
+        user=app.config['DBUSER'],
+        passwd=app.config['DBPASS'],
+        host=app.config['DBHOST'],
+        port=app.config['DBPORT'],
+        db=app.config['DBNAME']
+    )
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
+
+
+@app.route('/')
+def index():
+    return '<h1>man.</h1>'
+
+@app.route('/dbc')
+def createDB():
+    User.query.delete()
+    
+
+    test = User(username="test_username", email="hello@user.ch")
+    db.session.add(test)
+    db.session.commit()
+
+    return "Created used table"
+
+@app.route('/users/')
+def users():
+    
+    
+    return str(User.query.all())
--- python/flask/code/instance/config.py
+++ python/flask/code/instance/config.py
@@ -1,1 +1,7 @@
 DEBUG = True
+
+DBUSER = 'postgres'
+DBPASS = 'test'
+DBHOST = 'db'
+DBPORT = '5432'
+DBNAME = 'flaskdb'
